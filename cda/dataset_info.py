1880df6291c8182c541f4b33ad8047eba6c7cbd2
alisoltanirad <soltaniradali@gmail.com>
2020-11-19 15:40:37 +0330

    Debug

--- cda/dataset_info.py
+++ cda/dataset_info.py
@@ -1,9 +1,9 @@
-from .college_scorecard import MetaData
+from .college_scorecard.api import MetaData
 
 class DatasetInfo():
 
     def __init__(self):
-        _dataset_info = MetaData()
+        self._dataset_info = MetaData()
 
     def print_attributes(self):
         print('Attributes:\n', self._dataset_info.get_attribute_names())
