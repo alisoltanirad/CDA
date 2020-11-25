bef9549b4ac61c163f10569b9fb710850bdfa0fd
alisoltanirad <soltaniradali@gmail.com>
2020-11-25 14:44:32 +0330

    Add plot highest degrees to college info

--- cda/college_info.py
+++ cda/college_info.py
@@ -1,9 +1,24 @@
+from matplotlib import pyplot as plt
 from .college_scorecard import Dataset
 
 class CollegeInfo():
 
     def __init__(self):
-        self._dataset = Dataset()
+        self._dataset = Dataset(path='cda/college_scorecard/college_scorecard.csv')
 
     def show_colleges_info(self):
         print(self._dataset.colleges())
+
+    def plot_highest_degrees(self):
+        data = self._dataset.colleges()['Highest_Degree'].value_counts()
+
+        degrees, numbers = [], []
+        for degree, number in data.items():
+            degrees.append(degree)
+            numbers.append(number)
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(numbers, labels=degrees, autopct='%1.1f%%', startangle=90)
+        ax1.axis('equal')
+        plt.title('Colleges Highest Degree Awarded')
+        plt.show()
