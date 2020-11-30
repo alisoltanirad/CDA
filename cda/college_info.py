9b82d44c61b51d37c165f2b64f5913c16f3142d6
alisoltanirad <soltaniradali@gmail.com>
2020-11-30 16:15:02 +0330

    Add plot gender exclusive colleges colleges method

--- cda/college_info.py
+++ cda/college_info.py
@@ -9,6 +9,19 @@ class CollegeInfo():
     def show_data(self):
         print(self._data)
 
+    def plot_gender_exclusive_colleges(self):
+        men_only = self._data['Is_Men_Only'].value_counts()['true']
+        women_only = self._data['Is_Women_Only'].value_counts()['true']
+        other = len(self._data.index) - (men_only + women_only)
+
+        categories = ['Men-Only', 'Women-Only', 'Other']
+        numbers = [men_only, women_only, other]
+
+        fig, ax = plt.subplots()
+        ax.bar(categories, numbers)
+        plt.title('Gender Exclusive Colleges')
+        plt.show()
+
     def plot_highest_degrees(self):
         data = self._data['Highest_Degree'].value_counts()
 
@@ -17,8 +30,8 @@ class CollegeInfo():
             degrees.append(degree)
             numbers.append(number)
 
-        fig1, ax1 = plt.subplots()
-        ax1.pie(numbers, labels=degrees, autopct='%1.1f%%', startangle=90)
-        ax1.axis('equal')
+        fig, ax = plt.subplots()
+        ax.pie(numbers, labels=degrees, autopct='%1.1f%%', startangle=90)
+        ax.axis('equal')
         plt.title('Colleges Highest Degree Awarded')
         plt.show()
