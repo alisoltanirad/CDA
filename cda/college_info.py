7d4ced554721aed93f1bb917375daa0f2b9a6993
alisoltanirad <soltaniradali@gmail.com>
2020-12-01 15:26:16 +0330

    Refactor code

--- cda/college_info.py
+++ cda/college_info.py
@@ -10,12 +10,7 @@ class CollegeInfo():
         print(self._data)
 
     def plot_gender_exclusive_colleges(self):
-        men_only = self._data['Is_Men_Only'].value_counts()['true']
-        women_only = self._data['Is_Women_Only'].value_counts()['true']
-        other = len(self._data.index) - (men_only + women_only)
-
-        categories = ['Men-Only', 'Women-Only', 'Other']
-        numbers = [men_only, women_only, other]
+        categories, numbers = self._get_gender_exclusive_info()
 
         fig, ax = plt.subplots()
         ax.bar(categories, numbers)
@@ -23,6 +18,25 @@ class CollegeInfo():
         plt.show()
 
     def plot_highest_degrees(self):
+        degrees, numbers = self._get_highest_degrees_info()
+
+        fig, ax = plt.subplots()
+        ax.pie(numbers, labels=degrees, autopct='%1.1f%%', startangle=90)
+        ax.axis('equal')
+        plt.title('Colleges Highest Degree Awarded')
+        plt.show()
+
+    def _get_gender_exclusive_info(self):
+        men_only = self._data['Is_Men_Only'].value_counts()['true']
+        women_only = self._data['Is_Women_Only'].value_counts()['true']
+        other = len(self._data.index) - (men_only + women_only)
+
+        categories = ['Men-Only', 'Women-Only', 'Other']
+        numbers = [men_only, women_only, other]
+
+        return categories, numbers
+
+    def _get_highest_degrees_info(self):
         data = self._data['Highest_Degree'].value_counts()
 
         degrees, numbers = [], []
@@ -30,8 +44,4 @@ class CollegeInfo():
             degrees.append(degree)
             numbers.append(number)
 
-        fig, ax = plt.subplots()
-        ax.pie(numbers, labels=degrees, autopct='%1.1f%%', startangle=90)
-        ax.axis('equal')
-        plt.title('Colleges Highest Degree Awarded')
-        plt.show()
+        return degrees, numbers
