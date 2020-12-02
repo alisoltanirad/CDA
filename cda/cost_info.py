99e44e2570db6bf1307b8391eb3cba9c8ff82187
alisoltanirad <soltaniradali@gmail.com>
2020-12-02 15:07:09 +0330

    Add get net price average method:

--- cda/cost_info.py
+++ cda/cost_info.py
@@ -7,3 +7,16 @@ class CostInfo():
 
     def show_data(self):
         print(self._data)
+
+    def _get_net_price_avg(self):
+        net_price_avg_public = self._data.loc[
+            self._data['Ownership'] == 'public'
+        ]['Net_Price'].astype(float).mean()
+        net_price_avg_private = self._data.loc[
+            self._data['Ownership'] == 'private'
+        ]['Net_Price'].astype(float).mean()
+
+        categories = ['Public-Schools', 'Private-Schools']
+        numbers = [net_price_avg_public, net_price_avg_private]
+
+        return categories, numbers
