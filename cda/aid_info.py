628136c2b1b643142151b81894e0d9ef25edf743
alisoltanirad <soltaniradali@gmail.com>
2020-12-02 17:32:14 +0330

    Add get avg debt method

--- cda/aid_info.py
+++ cda/aid_info.py
@@ -6,4 +6,35 @@ class FinancialAidInfo():
         self._data = FinancialData().get_aid_info()
 
     def show_data(self):
-        print(self._data)
+        print(self._data['Debt_Dependent'])
+
+    def get_avg_debt(self):
+        data = {
+            'Overall': float('{:.2f}'.format(
+                self._data['Debt_Overall'][
+                    self._data['Debt_Overall'].values != 'PrivacySuppressed'
+                ].astype(float).mean()
+            )),
+            'Completers': float('{:.2f}'.format(
+                self._data['Debt_Completers'][
+                    self._data['Debt_Completers'].values != 'PrivacySuppressed'
+                ].astype(float).mean()
+            )),
+            'NonCompleters': float('{:.2f}'.format(
+                self._data['Debt_NonCompleters'][
+                    self._data['Debt_NonCompleters'].values != 'PrivacySuppressed'
+                ].astype(float).mean()
+            )),
+            'Dependent': float('{:.2f}'.format(
+                self._data['Debt_Dependent'][
+                    self._data['Debt_Dependent'].values != 'PrivacySuppressed'
+                ].astype(float).mean()
+            )),
+            'Independent': float('{:.2f}'.format(
+                self._data['Debt_Independent'][
+                    self._data['Debt_Independent'].values != 'PrivacySuppressed'
+                ].astype(float).mean()
+            )),
+        }
+
+        return data
