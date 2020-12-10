f8799b1201407a3d6e505794028509e26699cdff
alisoltanirad <soltaniradali@gmail.com>
2020-12-10 14:55:56 +0330

    Remove build files from git

--- build/lib/cda/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dataset_info import DatasetInfo
-from .college_info import CollegeInfo
-from .student_info import StudentInfo
-from .cost_info import CostInfo
-from .aid_info import FinancialAidInfo
-from .college_evaluation import CollegeEvaluation
--- build/lib/cda/college_evaluation.py
+++
@@ -1,9 +0,0 @@
-from .college_scorecard import CollegeData
-
-class CollegeEvaluation():
-
-    def __init__(self):
-        self._data = CollegeData().get_evaluation_metrics()
-
-    def export_data(self, path='evaluation_data.csv'):
-        self._data.to_csv(path, index=False)
