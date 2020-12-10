302abf2b4539635be8d25d5803df3199bf078744
alisoltanirad <soltaniradali@gmail.com>
2020-12-10 13:37:51 +0330

    Add plot_tuition_sat method

--- cda/college_evaluation.py
+++ cda/college_evaluation.py
@@ -1,9 +1,23 @@
+import matplotlib.pyplot as plt
 from .college_scorecard import CollegeData
 
 class CollegeEvaluation():
 
     def __init__(self):
-        self._data = CollegeData().get_evaluation_metrics()
+        self._college_data = CollegeData()
+        self._college_info = self._college_data.get_info()
+        self._evaluation_metrics = self._college_data.get_evaluation_metrics()
 
     def export_data(self, path='evaluation_data.csv'):
-        self._data.to_csv(path, index=False)
+        self._evaluation_metrics.to_csv(path, index=False)
+
+    def plot_tuition_sat(self):
+        tuition = self._college_info['Tuition_Revenue'].astype(float)
+        sat_scores = self._evaluation_metrics['SAT_Scores_Overall'].astype(float)
+
+        fig, ax = plt.subplots()
+        ax.scatter(tuition, sat_scores)
+        plt.title('SAT scores correlation with tuition revenue')
+        plt.xlabel('Tuition Revenue (Avg.)')
+        plt.ylabel('SAT Scores (Overall)')
+        plt.show()
