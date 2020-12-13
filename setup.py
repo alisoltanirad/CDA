ccced341671d3dc38b3fe3769eb809fe27246972
alisoltanirad <soltaniradali@gmail.com>
2020-12-13 14:04:14 +0330

    Update documentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="CDA",
+    name="cda",
     version="0.1.0",
     description="College Data Analysis",
     long_description=long_description,
