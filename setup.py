88c43c9bd63e27678c9d1efedc267f052184013e
alisoltanirad <soltaniradali@gmail.com>
2020-12-13 14:07:22 +0330

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cda",
-    version="0.1.0",
+    version="0.1.1",
     description="College Data Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
